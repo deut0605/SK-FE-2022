c3765e18e9a280019e5bcf3dc3e4c8e316a4255e
yamoo9 <yamoog@naver.com>
2022-05-10 15:11:00 +0900

    learn. ES.Next
    - ES Module
    - export
    - import

--- src/utils/currency/numberWithComma.js
+++ src/utils/currency/numberWithComma.js
@@ -1,4 +1,9 @@
 // numberWithComma 함수를 "기본 내보내기"로 내보냅니다.
 
+// 이름으로 내보내기 (named export)
+// export const numberWithComma = (n) =>
 const numberWithComma = (n) =>
   n.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+
+// 기본 내보내기 (default export)
+export default numberWithComma;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,16 @@
+// RE-EXPORT
+
+/* eslint-disable no-unused-vars */
 // 1. numberWithComma 모듈을 다시 내보냅니다.
+import numberWithComma from './currency/numberWithComma.js';
 
 // 2. currency, currencyKR 모듈을 다시 내보냅니다.
+import { currency, currencyKR } from './currency/currency.js';
+
+const utils = {
+  numberWithComma,
+  currency,
+  currencyKR,
+};
+
+export default utils;
