ba5f0cc40a3ae2d29b1742bbb9e243151006148a
yamoo9 <yamoog@naver.com>
2022-05-12 15:19:20 +0900

    learn. CSS Modules 활용
    - webpack + css loader option `module` 활성
    - CRA 툴체인에서는 파일 명: `filename.module.css`
    - classes 모듈(객체)로 활용

--- presentation-component/src/App.js
+++ presentation-component/src/App.js
@@ -1,18 +1,14 @@
-import { A11yHidden } from './components/A11yHidden/A11yHidden';
+import { A11yHidden } from 'components';
 
 export default function App() {
   return (
     <div className="container">
-      <A11yHidden
-        as="a"
-        href="https://google.com"
-        focusable
-        className="designerClass"
-      >
-        <h1>Banner 컴포넌트가 이 곳에 위치해야 합니다.</h1>
+      <A11yHidden as="h1" className="a b c d" focusable>
+        Banner 컴포넌트가 이 곳에 위치해야 합니다.
       </A11yHidden>
-
-      <h2 className="a11yHidden">Banner 컴포넌트가 이 곳에 위치해야 합니다.</h2>
+      <A11yHidden as="a" href="https://google.com" external focusable>
+        Banner 컴포넌트가 이 곳에 위치해야 합니다.
+      </A11yHidden>
     </div>
   );
 }
--- presentation-component/src/components/A11yHidden/A11yHidden.js
+++ presentation-component/src/components/A11yHidden/A11yHidden.js
@@ -1,21 +1,22 @@
-import './A11yHidden.css';
+import styles from './A11yHidden.module.css';
 import React from 'react';
 import classNames from 'classnames';
 
-export const A11yHidden = ({ as, focusable, className, ...restProps }) => {
-  const Comp = as;
-
-  return (
-    <Comp
-      className={classNames('a11yHidden', { focusable }, className)}
-      {...restProps}
-    />
-  );
-};
-
-// export const A11yHidden = ({ as, focusable, className, ...restProps }) => {
-//   return React.createElement(as, {
-//     className: classNames('a11yHidden', { focusable }, className),
-//     ...restProps,
-//   });
-// };
+export const A11yHidden = ({
+  as: Comp = 'span',
+  focusable,
+  className,
+  external,
+  ...restProps
+}) => (
+  <Comp
+    className={classNames(
+      styles.container,
+      focusable ? styles.focusable : '',
+      className
+    )}
+    target={external && '_blank'}
+    rel={external && 'noopener noreferrer'}
+    {...restProps}
+  />
+);
